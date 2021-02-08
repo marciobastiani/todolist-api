4e48b881d3588732c00e82ba3325df1c2e946b30
Marcio de Bastiani Silva <marciobsjau@gmail.com>
2021-02-07 22:22:04 -0300

    Mensagem customizada
    Para Model Not Found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,17 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException) {
+            $modelName = class_basename($exception->getModel());
+            $apiErrorCode = $modelName . 'NotFoundException';
+            $message = $modelName . ' Not Found';
+
+            return response()->json([
+                'error' => $apiErrorCode,
+                'message' => $message
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
